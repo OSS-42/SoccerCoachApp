9d8eb236b87a2a427f34e5bfcd27fa1d2a96d17b
OSS-42 <eric.wursteisen@gmail.com>
2026-03-15 13:53:06 -0400

    button layout update.

--- dist_v2/js/core/constants.js
+++ dist_v2/js/core/constants.js
@@ -1,6 +1,6 @@
 // Core constants and configuration for Soccer Coach Tracker
 window.APP_CONFIG = {
-    APP_VERSION: 'v1.12.77',
+    APP_VERSION: 'v1.12.78',
     HAPTIC: {
         PICK_MS: 50,
         HOVER_MS: 30
